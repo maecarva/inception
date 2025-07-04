91580225cc189fd9443e373ed9a29293fb31ae60
admin <maecarva@Inception.myguest.virtualbox.org>
2025-07-04 18:42:57 +0200

    mandatory ok

--- Makefile
+++ Makefile
@@ -6,11 +6,14 @@ up:
 	docker compose -f srcs/docker-compose.yml up --build -d
 
 down:
-	docker compose -f srcs/docker-compose.yml down
+	docker compose -f srcs/docker-compose.yml down -v
 
 fclean:
-	# docker volume rm -f `docker volume ls -q`
+	docker volume rm -f `docker volume ls -q`
 	docker images -q | xargs -r docker rmi -f
-	# docker network rm -f `docker network ls -q`
+	docker network rm -f `docker network ls -q`
 
-.PHONY: all up down
+re: down fclean all
+	
+
+.PHONY: all up down re
--- srcs/requirements/wordpress/setup.sh
+++ srcs/requirements/wordpress/setup.sh
@@ -1,11 +1,23 @@
-wp config create --dbname=wordpress --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost=mariadb --force
+echo "[WP config] Waiting for MariaDB..."
+until mariadb-admin ping -h "${DB_HOST}" -u "${DB_USER}" -p"${DB_PASSWORD}" --silent; do
+	echo "cant contact db"
+	sleep 3
+done
+echo "[WP config] MariaDB accessible."
 
-wp core is-installed || wp core install --url=maecarva.42.fr --title="InceptionPress" --admin_user="$WP_USER1" \
+
+wp config create --dbname=inceptiondb --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost=mariadb:3306 --force
+
+wp core is-installed || wp core install --url=maecarva.42.fr --title="Inception" --admin_user="$WP_USER1" \
 	--admin_password="$WP_PASS1" --admin_email="$WP_USER1"@mail.org --skip-email
 
 
 wp user create "$WP_USER2" "$WP_USER2"@mail.org --user_pass="$WP_PASS2"
 
-wp theme install inspiro --activate
+wp theme install twentytwentyone --activate
 
-exec php-fpm81 -F
+wp plugin install redis-cache --activate
+wp config set WP_REDIS_HOST "redis"
+wp redis enable
+
+exec php-fpm83 -F
